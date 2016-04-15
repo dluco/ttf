695608a528f624006172f9b215c8778a43bc2c6a
David Luco <dluco11@gmail.com>
2016-04-15 11:59:52 -0700

    Add more cmdline opts.

--- main.c
+++ main.c
@@ -5,18 +5,30 @@
 #include <math.h>
 
 #define FONT_FILENAME "data/Vera.ttf"
+#define OUTPUT_FILE "data/output.png"
+#define FONT_SIZE 12
+#define SCREEN_DPI 96
 
 int main(int argc, char* argv[]) {
 	char *font_filename = FONT_FILENAME;
+	int font_size = FONT_SIZE;
+	int screen_dpi = SCREEN_DPI;
 	int render_method = RENDER_FP;
 	int apply_gamma = 0;
 	float gamma = 1.00;
+	char *output_file = OUTPUT_FILE;
 
 	int c;
-	while ((c = getopt(argc, argv, "f:m:g:")) != -1) {
+	while ((c = getopt(argc, argv, "f:s:d:m:g:o:")) != -1) {
 		switch (c) {
 			case 'f':
 				font_filename = optarg;
+				break;
+			case 's':
+				font_size = atoi(optarg);
+				break;
+			case 'd':
+				screen_dpi = atoi(optarg);
 				break;
 			case 'm':
 				if (strcmp(optarg, "fp") == 0) {
@@ -34,6 +46,9 @@ int main(int argc, char* argv[]) {
 				gamma = atof(optarg);
 				apply_gamma = 1;
 				break;
+			case 'o':
+				output_file = optarg;
+				break;
 			default:
 				break;
 		}
@@ -47,7 +62,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	TTF_Font *font = load_font(font_filename);
-	raster_init(font, 12, 96, render_method);
+	raster_init(font, font_size, screen_dpi, render_method);
 
 	/* Calculate required size for output bitmap. */
 	int16_t ascent = funit_to_pixel(font, get_font_ascent(font));
@@ -64,7 +79,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	if (out) {
-		save_bitmap(out, "data/output.png", NULL);
+		save_bitmap(out, output_file, NULL);
 		free_bitmap(out);
 	}
 
